c950578fbc28a4c46cdf078b602a9d222966bca6
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2021-04-25 01:28:46 +0200

    electron build fixes

--- src/tools/ledger/ledgerElectronAPI.ts
+++ src/tools/ledger/ledgerElectronAPI.ts
@@ -4,26 +4,29 @@ import { ILedgerTransaction } from "../../types/LedgerDelegationTransaction";
 
 // Wrapped Electron API for Ledger Devices
 
-if (isElectron()) {
-  // Import the node-hid
-  ipcRenderer = window.require("electron").ipcRenderer;
-}
+// if (isElectron()) {
+//   // Import the node-hid
+//   ipcRenderer = window.require("electron").ipcRenderer;
+// }
 
 export const isMinaAppOpen = async () => {
   if (!isElectron()) {
     throw new Error("Wrong environment");
   }
+  const { ipcRenderer } = await import("electron");
   return await ipcRenderer.invoke("ledger-get-name-version");
 };
 export const getPublicKey = async (account: number) => {
   if (!isElectron()) {
     throw new Error("Wrong environment");
   }
+  const { ipcRenderer } = await import("electron");
   return await ipcRenderer.invoke("ledger-get-address", account);
 };
 export const signTransaction = async (transaction: ILedgerTransaction) => {
   if (!isElectron()) {
     throw new Error("Wrong environment");
   }
+  const { ipcRenderer } = await import("electron");
   return await ipcRenderer.invoke("ledger-sign-transaction", transaction);
 };
