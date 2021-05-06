b4c75da61dd82434ff628fc305e9dfee09dc9841
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2021-05-07 01:03:26 +0200

    fixed wallet id update

--- src/tools/const/system.ts
+++ src/tools/const/system.ts
@@ -2,3 +2,5 @@
 export const DEFAULT_QUERY_REFRESH_INTERVAL = 30 * 1000;
 // 5s delay (5000ms)
 export const TOAST_TIMEOUT = 5 * 1000;
+// 10s delay (10000ms)
+export const UPDATE_WALLET_ID_TIMEOUT = 10 * 1000;
