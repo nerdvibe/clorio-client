9c127f5f700451f81f3e3184babe8f439f439364
nerdvibe <6796782+nerdvibe@users.noreply.github.com>
2021-05-31 01:40:30 +0200

    enabled contextIsolation

--- public/preload.js
+++ public/preload.js
@@ -1,8 +1,13 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, contextBridge } = require("electron");
 
 function init() {
   window.isElectron = true;
   window.ipcRenderer = ipcRenderer;
+  contextBridge.exposeInMainWorld("ipcBridge", {
+    invoke: (text) => {
+      return ipcRenderer.invoke(text);
+    },
+  });
 }
 
 init();
