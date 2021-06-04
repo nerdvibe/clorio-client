56d22b35a248c081a130fa7507fdb2cb7ef49623
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2021-06-04 02:09:34 +0200

    Added unauthenticated routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,20 +5,16 @@ const Stake = React.lazy(() => import("./pages/stake/Stake"));
 const SplashScreen = React.lazy(() => import("./pages/SplashScreen"));
 const Login = React.lazy(() => import("./pages/Login"));
 const Register = React.lazy(() => import("./pages/registration/Registration"));
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { AuthenticatedRoute } from "./components/routes/AuthenticatedRoute";
+import { UnauthenticatedRoute } from "./components/routes/UnauthenticatedRoute";
 import Spinner from "./components/UI/Spinner";
 const LedgerLogin = React.lazy(() => import("./pages/LedgerLogin"));
 const SignMessage = React.lazy(() => import("./pages/signMessage/SignMessage"));
 const NotFound = React.lazy(() => import("./pages/404"));
 const VerifyMessage = React.lazy(() => import("./pages/VerifyMessage"));
-import { isEmptyObject } from "./tools/utils";
 import { INetworkData } from "./types/NetworkData";
 import { IWalletData } from "./types/WalletData";
-
-interface IProtectedRouteProps {
-  component: React.FC<any>;
-  [key: string]: any;
-}
 
 interface IRoutesProps {
   sessionData: IWalletData;
@@ -26,81 +22,70 @@ interface IRoutesProps {
   toggleLoader: (state?: boolean) => void;
 }
 
-const ProtectedRoute = ({
-  component: Component,
-  ...rest
-}: IProtectedRouteProps) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (
-          rest.sessionData &&
-          !isEmptyObject(rest.sessionData) &&
-          rest.sessionData?.address
-        ) {
-          return (
-            <Component {...props} {...rest} sessionData={rest.sessionData} />
-          );
-        } else {
-          return (
-            <Redirect
-              to={{
-                pathname: "/",
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
-        }
-      }}
-    />
-  );
-};
-
 const Routes = (props: IRoutesProps) => {
   return (
     <Suspense fallback={<Spinner show={true} />}>
       <Switch>
-        <ProtectedRoute
-          exact
+        <AuthenticatedRoute
           path="/overview"
-          component={Overview}
-          {...props}
+          Component={Overview}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
         />
-        <ProtectedRoute exact path="/send-tx" component={SendTX} {...props} />
-        <ProtectedRoute exact path="/stake" component={Stake} {...props} />
-        <ProtectedRoute
-          exact
+        <AuthenticatedRoute
+          path="/send-tx"
+          Component={SendTX}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
+        <AuthenticatedRoute
+          path="/stake"
+          Component={Stake}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
+        <AuthenticatedRoute
           path="/sign-message"
-          component={SignMessage}
-          {...props}
+          Component={SignMessage}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
         />
-        <ProtectedRoute
-          exact
+        <AuthenticatedRoute
           path="/verify-message"
-          component={VerifyMessage}
-          {...props}
+          Component={VerifyMessage}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
         />
-        <Route path="/login" exact>
-          <Login {...props} network={props.network} />
-        </Route>
-        <Route path="/register" exact>
-          <Register {...props} network={props.network} />
-        </Route>
-        <Route path="/ledger" exact>
-          <LedgerLogin {...props} network={props.network} />
-        </Route>
+        <UnauthenticatedRoute
+          path="/login"
+          Component={Login}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
+        <UnauthenticatedRoute
+          path="/register"
+          Component={Register}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
+        <UnauthenticatedRoute
+          path="/ledger"
+          Component={LedgerLogin}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
+        <UnauthenticatedRoute
+          path="/"
+          Component={SplashScreen}
+          appProps={props}
+          isAuthenticated={!!props.sessionData.address}
+        />
         {/*<Route path="/register">
           <Entropy />
         </Route>*/}
         {/*<Route path="/verify">
           <VerifyMnemonic />
         </Route>*/}
-        <Route path="/" exact>
-          <SplashScreen network={props.network} />
-        </Route>
         <Route component={NotFound} />
       </Switch>
     </Suspense>
