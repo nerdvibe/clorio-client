809559f2acee575fa74692baf011e87e4a41593b
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2021-06-07 20:05:28 +0200

    Added clorio app banner

---
+++ src/components/UI/ClorioAppBanner.tsx
@@ -0,0 +1,31 @@
+import isElectron from "is-electron";
+import { useState } from "react";
+import { X } from "react-feather";
+
+export const ClorioAppBanner = () => {
+  const [show, setShow] = useState(true);
+  return !isElectron() && show ? (
+    <div className="clorio-app-banner">
+      <div>
+        <div>
+          <p>To enhance your security we recommend to download Clorio App.</p>
+          <p>
+            For more info click{" "}
+            <a
+              href={`https://github.com/nerdvibe/clorio-client/releases`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              here.
+            </a>
+          </p>
+        </div>
+        <div className="close-button" onClick={() => setShow(false)}>
+          <X />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+};
