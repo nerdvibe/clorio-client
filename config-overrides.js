3445d56ce9f12eb95f044ba95cd7adf302d6667f
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2021-12-14 04:16:10 +0100

    CSP Update

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,10 @@ const { override } = require("customize-cra");
 const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
-  "default-src": "'self' https://*.clor.io https://fonts.googleapis.com",
-  "connect-src": "'self' https://*.clor.io/v1/graphql",
+  "default-src":
+    "'self' https://*.clor.io https://fonts.googleapis.com https://api.mina.tools",
+  "connect-src":
+    "'self' https://*.clor.io/v1/graphql https://api.mina.tools/v1/epoch",
   "img-src": "https://*.staketab.com 'self'",
   "script-src": "'self'",
   "style-src": "'self' https://fonts.googleapis.com",
