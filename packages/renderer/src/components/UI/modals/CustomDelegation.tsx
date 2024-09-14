9883bb340bd5debeb079f19f76c41e8554079dda
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2024-09-13 21:10:14 -0800

    Added address book to the custom delegate form

--- packages/renderer/src/components/UI/modals/CustomDelegation.tsx
+++ packages/renderer/src/components/UI/modals/CustomDelegation.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { ArrowLeft, ArrowRight } from 'react-feather';
+import {useState} from 'react';
+import {Row, Col} from 'react-bootstrap';
+import {ArrowLeft, ArrowRight} from 'react-feather';
 import Button from '../Button';
 import Input from '../input/Input';
+import {ModalContainer} from './ModalContainer';
+import AddressBook from './AddressBook';
 
 interface IProps {
   closeModal: () => void;
   confirmCustomDelegate: (customDelegate: string) => void;
 }
 
-export const ConfirmCustomDelegation = ({
-  closeModal,
-  confirmCustomDelegate,
-}: IProps) => {
+export const ConfirmCustomDelegation = ({closeModal, confirmCustomDelegate}: IProps) => {
   const [customDelegate, setCustomDelegate] = useState<string>('');
+  const [showAddressBookModal, setShowAddressBookModal] = useState(false);
+
+  const toggleAddressBookModal = () => setShowAddressBookModal(!showAddressBookModal);
+
   return (
     <div className="min-width-500">
       <div className="w-100">
         <div className="flex flex-col flex-vertical-center">
           <h1 className="mb-0">Custom delegation </h1>
-          <p className="text-center mt-1 mb-1">
-            Delegate to someone that is not in the list
-          </p>
+          <p className="text-center mt-1 mb-1">Delegate to someone that is not in the list</p>
           <div className="divider w-100" />
         </div>
       </div>
 
       <div className="align-left mt-1 mb-2 label">
-        <strong>Public key</strong>
+        <div className="flex flex-row justify-between">
+          <strong>Public key</strong>
+          <Button
+            className="link-button custom-delegate-button purple-text align-end  no-padding"
+            text="Address book"
+            onClick={toggleAddressBookModal}
+          />
+        </div>
       </div>
       <Input
-        inputHandler={(e) => {
+        value={customDelegate}
+        inputHandler={e => {
           setCustomDelegate(e.currentTarget.value);
         }}
         placeholder="Insert public key"
@@ -55,6 +64,19 @@ export const ConfirmCustomDelegation = ({
           />
         </Col>
       </Row>
+      <ModalContainer
+        show={showAddressBookModal}
+        close={toggleAddressBookModal}
+        closeOnBackgroundClick
+      >
+        <AddressBook
+          selectable
+          onSelect={address => {
+            setCustomDelegate(address);
+            toggleAddressBookModal();
+          }}
+        />
+      </ModalContainer>
     </div>
   );
 };
