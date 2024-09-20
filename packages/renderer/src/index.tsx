a0256a87512c781771927ce406f5802433c67f78
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2024-09-20 11:30:47 -0800

    Added base i18n config

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {initHtmlElements} from './tools';
 import {NetworkSettingsProvider} from './contexts/NetworkContext';
 import {RecoilRoot} from 'recoil';
+import {I18nextProvider} from 'react-i18next';
+import i18n from './lang/i18n';
 globalThis.Buffer = Buffer;
 
 // Render your React component instead
@@ -12,8 +14,10 @@ const root = createRoot(document.getElementById('root'));
 initHtmlElements();
 root.render(
   <NetworkSettingsProvider>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <I18nextProvider i18n={i18n}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </I18nextProvider>
   </NetworkSettingsProvider>,
 );
---
+++ packages/renderer/src/lang/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from './en.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+    },
+  });
+
+export default i18n;
