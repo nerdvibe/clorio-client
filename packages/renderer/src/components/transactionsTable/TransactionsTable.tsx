37699771a976fa3f9680831eabb81e2fdff2f6a8
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2024-10-23 19:11:43 +0200

    Added mempool tx and wallet creation fee to tx table

--- packages/renderer/src/components/transactionsTable/TransactionsTable.tsx
+++ packages/renderer/src/components/transactionsTable/TransactionsTable.tsx
@@ -1,38 +1,30 @@
-import {Badge, Table} from 'react-bootstrap';
+import {Badge} from 'react-bootstrap';
 import Spinner from '../UI/Spinner';
-import {getTotalPages, openLinkOnBrowser, sanitizeString, trimMiddle} from '../../tools/utils';
+import {
+  copyToClipboard,
+  getTotalPages,
+  openLinkOnBrowser,
+  sanitizeString,
+  trimMiddle,
+} from '../../tools/utils';
 import {useQuery} from '@apollo/client';
 import Pagination from '../UI/pagination/Pagination';
 import ReactTooltip from 'react-tooltip';
 import {GET_BLACKLIST, GET_TRANSACTIONS_TOTAL} from '../../graphql/query';
-import type {
-  ITransactionRowData,
-  ITransactionTableProps,
-  ITransactionTotalQueryResult,
-} from './TransactionsTypes';
-import TransactionRow from './TransactionRow';
+import type {ITransactionTableProps, ITransactionTotalQueryResult} from './TransactionsTypes';
 import TransactionsTableError from './TransactionsTableError';
-import {
-  formatUrl,
-  mempoolQueryRowToTableRow,
-  transactionQueryRowToTableRow,
-} from './TransactionsHelper';
+import {formatUrl} from './TransactionsHelper';
 import WalletCreationTransaction from './WalletCreationTransaction';
 import RefetchTransactions from './RefetchTransactions';
 import type {Blacklist} from '../../types/Blacklist';
-import {
-  getTimeDistance,
-  humanAmount,
-  isScamTransaction,
-  toMINA,
-  TRANSACTIONS_TABLE_ITEMS_PER_PAGE,
-  trimAddress,
-} from '/@/tools';
+import {getTimeDistance, humanAmount, isScamTransaction, toMINA, trimAddress} from '/@/tools';
 import DataTable from 'react-data-table-component';
 import TransactionIcon from './TransactionIcon';
 import {useNetworkSettingsContext} from '/@/contexts/NetworkContext';
 import Avatar from '/@/tools/avatar/avatar';
-import {ArrowRight} from 'react-feather';
+import {ArrowRight, Copy} from 'react-feather';
+import Button from '../UI/Button';
+
 const customStyles = {
   rows: {
     highlightOnHoverStyle: {
@@ -131,10 +123,10 @@ const TransactionsTable = ({
         const color = isScam
           ? 'danger'
           : isMempool
-          ? 'secondary'
+          ? 'secondary-alt'
           : failure_reason
-          ? 'danger'
-          : 'success';
+          ? 'danger-alt'
+          : 'success-alt';
         blacklist?.blacklistedAddresses?.reduce((previous, actual) => {
           if (actual.address === receiver_public_key) {
             senderScam = 1;
@@ -146,28 +138,47 @@ const TransactionsTable = ({
           );
         }, false);
         return (
-          <Badge
-            bg={color}
-            className="beta-tag medium-table-value-text"
-          >
-            {isScam ? 'Scam' : isMempool ? 'Pending' : failure_reason ? 'Failed' : 'Confirmed'}
-          </Badge>
+          <div className="flex flex-col justify-center items-start gap-2">
+            <Badge
+              bg={color}
+              className="medium-table-value-text"
+            >
+              {isScam ? 'Scam' : isMempool ? 'Pending' : failure_reason ? 'Failed' : 'Confirmed'}
+            </Badge>
+            {failure_reason && (
+              <div className="small-table-value-text opacity-5">{failure_reason}</div>
+            )}
+          </div>
         );
       },
     },
     {
       name: 'Hash',
-      minWidth: '250px',
+      minWidth: '280px',
       cell: row => (
         <div className="table-value-text flex flex-col flex-start">
-          <a
-            className="purple-hover table-value-text bold"
-            onClick={() =>
-              !row.isMempool && openLinkOnBrowser(formatUrl(row.hash, settings?.explorerUrl))
-            }
+          <div
+            className={`${
+              row.type !== 'create_account' ? 'purple-hover' : ''
+            } table-value-text bold flex justify-center items-center gap-2`}
           >
-            {trimMiddle(row.hash, 20)}
-          </a>
+            <span
+              onClick={() =>
+                row.type !== 'create_account' &&
+                !row.isMempool &&
+                openLinkOnBrowser(formatUrl(row.hash, settings?.explorerUrl))
+              }
+            >
+              {trimMiddle(row.hash, 20)}
+            </span>
+            {row.type !== 'create_account' && (
+              <Button
+                className="inline-element"
+                icon={<Copy size={20} />}
+                onClick={() => copyToClipboard(row.hash)}
+              />
+            )}
+          </div>
           <div className="small-table-value-text opacity-5">
             {row.memo ? `Memo: ${sanitizeString(row.memo)}` : null}
           </div>
@@ -262,7 +273,7 @@ const TransactionsTable = ({
     },
     {
       name: 'Amount',
-      width: '200px',
+      width: '150px',
       cell: ({sender_public_key, amount, receiver_public_key, command_type, fee}) => {
         const totalAmount = humanAmount(
           amount,
@@ -287,9 +298,9 @@ const TransactionsTable = ({
     },
     {
       name: 'Age',
-      cell: ({timestamp}) => (
+      cell: ({timestamp, type}) => (
         <span className="medium-table-value-text text-start">
-          {getTimeDistance(+timestamp, false)}
+          {type !== 'create_account' && getTimeDistance(+timestamp, false)}
         </span>
       ),
     },
@@ -307,67 +318,50 @@ const TransactionsTable = ({
     return TransactionsTableError(balance, error, refetchData);
   }
 
-  /**
-   * Render table header labels
-   * @returns HTMLElement
-   */
-  const renderTableHeader = () => {
-    return (
-      <tr className="th-background">
-        <th className="th-first-item"></th>
-        <th>ID</th>
-        <th>Date</th>
-        <th>Sender</th>
-        <th>Recipient</th>
-        <th className="th-last-item">Amount</th>
-      </tr>
-    );
-  };
-
-  /**
-   * Render table body content
-   * @returns HTMLElement
-   */
-  const renderTableBody = () => {
-    return (
-      <tbody>
-        {mempool?.mempool?.map((row, index) => {
-          const rowData: ITransactionRowData = mempoolQueryRowToTableRow(row);
-          return TransactionRow(
-            rowData,
-            index,
-            userAddress,
-            blacklist?.blacklistedAddresses || [],
-            true,
-          );
-        })}
-        {transactions?.transactions?.map((row, index) => {
-          const rowData: ITransactionRowData = transactionQueryRowToTableRow(row);
-          return TransactionRow(
-            rowData,
-            index,
-            userAddress,
-            blacklist?.blacklistedAddresses || [],
-            false,
-          );
-        })}
-        {lastTransaction()}
-      </tbody>
-    );
-  };
-
-  /**
-   * If the last page of the table is rendered return an additional transaction row for the wallet creation
-   * @returns HTMLElement
-   */
-  const lastTransaction = () => {
-    const totalRows = totalData?.transactionsCount?.count || 0;
-    const isLastPage = +page === +getTotalPages(totalRows);
-    if (transactions?.transactions?.length) {
-      if (transactions?.transactions?.length < TRANSACTIONS_TABLE_ITEMS_PER_PAGE || isLastPage) {
-        return WalletCreationTransaction(totalRows + 1);
+  const formatMempool = () => {
+    const result = [];
+    if (page === 1) {
+      if (mempool?.mempool && mempool?.mempool?.length > 0) {
+        mempool?.mempool?.map(row => {
+          result.push({
+            ...row,
+            type: 'mempool',
+            sender_public_key: row.source?.publicKey,
+            receiver_public_key: row.receiver?.publicKey,
+            amount: row.amount,
+            hash: row.id,
+            timestamp: row.timestamp,
+            status: 'mempool',
+            command_type: row.kind,
+            failure_reason: '',
+            isMempool: true,
+          });
+        });
       }
     }
+    return result;
+  };
+
+  const mergeTansactions = () => {
+    const returnData = [...formatMempool()];
+    for (let i = 0; i < transactions?.transactions?.length; i++) {
+      returnData.push(transactions?.transactions[i]);
+    }
+    if (page == getTotalPages(totalData?.transactionsCount?.count || 0)) {
+      returnData.push({
+        id: '0',
+        amount: 1000000000,
+        sender_public_key: userAddress,
+        receiver_public_key: '',
+        hash: 'Wallet creation fee',
+        status: 'confirmed',
+        command_type: 'create_account',
+        type: 'create_account',
+        failure_reason: '',
+        isMempool: false,
+      });
+    }
+    return returnData;
   };
 
   return (
@@ -381,28 +375,13 @@ const TransactionsTable = ({
           <ReactTooltip multiline={true} />
           <DataTable
             columns={columns}
-            data={transactions?.transactions || []}
-            // expandOnRowClicked
-            // expandableRows
-            // expandableIcon={() => <></>}
+            data={mergeTansactions()}
             highlightOnHover
             persistTableHead
             responsive
             subHeaderWrap
             customStyles={customStyles}
           />
-          {/* <div>
-          <div id="transaction-table">
-            <RefetchTransactions refetch={refetchData} />
-            <Table
-              className="animate__animated animate__fadeIn"
-              id="rwd-table-large"
-            >
-              <thead>{renderTableHeader()}</thead>
-              {renderTableBody()}
-            </Table>
-          </div>
-          </div> */}
           <Pagination
             page={page}
             setOffset={setOffset}
